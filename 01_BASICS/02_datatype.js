807d5f9a3e76763e54fb168b4f40cff660537581
VT <157911088+VaishnaviTyagi23@users.noreply.github.com>
2024-07-19 08:50:08 +0000

    datatype

---
+++ 01_BASICS/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS code as newer version
+
+//alert(3+3)// we are using nodejs if this is done in browser it gives result as popup
+
+//READABILITY of code should be high
+console.log(3+3); console.log("Hello");//its right but not readable
+
+
+//datatype
+let name="hitesh"
+let age= 18
+let isLoggedIn= false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+// null=> standalone value(cant be assigned)
+//undefined =>
+// symbol => uniqueness
+
+//object
+
+console.log(typeof age);//number
+console.log(typeof null);//its an object
+console.log(typeof undefined);//undefined
